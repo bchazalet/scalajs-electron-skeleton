4e28b88c1da1a3d1d26a9b8a879e65651651ff4f
Boris Chazalet <boris.chazalet@gmail.com>
2015-07-20 22:12:49 +0100

    Listing the files in the current directory

--- scalajs/src/main/scala/com/example/electronapp/ElectronApp.scala
+++ scalajs/src/main/scala/com/example/electronapp/ElectronApp.scala
@@ -1,13 +1,29 @@
 package com.example.electronapp
 
-import scala.scalajs.js.JSApp
-
+import scala.scalajs.js
 import org.scalajs.jquery.jQuery
+import js.Dynamic.{global => g}
 
-object TutorialApp extends JSApp {
+object ElectronApp extends js.JSApp {
+
+  val fs = g.require("fs")
 
   def main(): Unit = {
     jQuery("body").append("<p>Hello World from Scala.js</p>")
+    val filenames = listFiles(".")
+    display(filenames)
   }
 
+  def display(filenames: Seq[String]) = {
+    jQuery("body").append("<p>Listing the files in the '.' using io.js API:")
+    jQuery("body").append("<ul>")
+    filenames.foreach { filename =>
+      jQuery("body").append(s"<li>$filename</li>")
+    }
+    jQuery("body").append("</ul></p>")
+  }
+
+  def listFiles(path: String): Seq[String] = {
+    fs.readdirSync(path).asInstanceOf[js.Array[String]]
+  }
 }
