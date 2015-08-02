59a448e554bbcd54cd25039bc06a193c1f36c260
Boris Chazalet <boris.chazalet@gmail.com>
2015-08-02 14:30:56 +0100

    Facading BrowserWindow with a class

--- scalajs/src/main/scala/com/example/electronapp/Main.scala
+++ scalajs/src/main/scala/com/example/electronapp/Main.scala
@@ -15,7 +15,7 @@ object Main extends js.JSApp {
 
     // Keep a global reference of the window object, if you don't, the window will
     // be closed automatically when the JavaScript object is GCed.
-    var mainWindow: js.Dynamic = null;
+    var mainWindow: BrowserWindow = null;
 
     // Quit when all windows are closed.
     app.on("window-all-closed", { () =>
@@ -33,18 +33,18 @@ object Main extends js.JSApp {
       mainWindow = BrowserWindow(width = 800, height = 600)
 
       // and load the index.html of the app.
-      mainWindow.loadUrl("file://" + g.__dirname + "/index.html");
+      mainWindow.loadUrl("file://" + g.__dirname + "/index.html")
 
       // Open the devtools.
-      mainWindow.openDevTools();
+      mainWindow.openDevTools()
 
       // Emitted when the window is closed.
-      mainWindow.on("closed", { () =>
+      mainWindow.on("closed", () =>
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        mainWindow = null;
-      });
+        mainWindow = null
+      );
     });
 
   }
--- scalajs/src/main/scala/io/atom/electron/BrowserWindow.scala
+++ scalajs/src/main/scala/io/atom/electron/BrowserWindow.scala
@@ -7,24 +7,24 @@ import js.Dynamic.{global => g}
 /**
  * See http://electron.atom.io/docs/v0.30.0/api/browser-window/
  */
-// class BrowserWindow extends js.Object { //(options: BrowserWindowOptions) extends js.Object {
-//
-//   def loadUrl(url: String): Unit = js.native
-//
-//   def openDevTools(): Unit = js.native
-//
-//   def on(event: String, listener: js.Function0[Unit]): BrowserWindow = js.native
-//
-// }
+class BrowserWindow extends js.Object { //(options: BrowserWindowOptions) extends js.Object {
+
+  def loadUrl(url: String): Unit = js.native
+
+  def openDevTools(): Unit = js.native
+
+  def on(event: String, listener: js.Function0[Unit]): BrowserWindow = js.native
+
+}
 
 object BrowserWindow {
 
   val browserWindow = g.require("browser-window").asInstanceOf[js.Dynamic]
 
-  def apply(width: Int = 800, height: Int = 600, show: Boolean = true): js.Dynamic = {
+  def apply(width: Int = 800, height: Int = 600, show: Boolean = true): BrowserWindow = {
     js.Dynamic.newInstance(browserWindow)(
       js.Dynamic.literal(width = width, height = height, show = show)
-    )
+    ).asInstanceOf[BrowserWindow]
   }
 
 }
